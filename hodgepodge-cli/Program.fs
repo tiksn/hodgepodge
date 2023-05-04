e28eba6c80081bc7aa3cd95c70752c2b17389feb
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-05-05 00:23:34 +0200

    Print processes

--- hodgepodge-cli/Program.fs
+++ hodgepodge-cli/Program.fs
@@ -1,2 +1,7 @@
 ﻿open TIKSN.hodgepodge
 
+let processes = Platform.listProcess ()
+
+match processes with
+| Ok v -> Dumpify.DumpExtensions.Dump(v) |> ignore
+| Error _ -> printfn "Failed to get process list"
