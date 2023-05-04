49f9d249e4cf6875fad0c95c060e44d3c2e3aff2
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-05-05 00:15:04 +0200

    Implement setRootProcessParentToNone

--- hodgepodge/CrossPlatform.fs
+++ hodgepodge/CrossPlatform.fs
@@ -2,5 +2,16 @@ module TIKSN.hodgepodge.CrossPlatform
 
 let makeNullAsNone x =
     match x with
-        | Some null -> None
-        | _ -> x
+    | Some null -> None
+    | _ -> x
+
+let setRootProcessParentToNone (p: ProcessInfo) =
+    match p.ParentProcessId with
+    | Some pid ->
+        if pid = p.ProcessId then
+            { ProcessId = p.ProcessId
+              ParentProcessId = None
+              Path = p.Path }
+        else
+            p
+    | None -> p
--- hodgepodge/Platform.fs
+++ hodgepodge/Platform.fs
@@ -7,4 +7,5 @@ let listProcess () =
     match Environment.OSVersion.Platform with
     | PlatformID.Win32NT -> Ok(Windows.listProcess ())
     | _ -> Error(pair 1469762974 "Platform not Found")
+    |> Result.map (fun x -> x |> Seq.map CrossPlatform.setRootProcessParentToNone)
     |> Result.map (fun x -> x |> Seq.toList)
