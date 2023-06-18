43fce5cef423f4c6113b4e470766c48b64f99dbc
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-06-19 02:11:00 +0300

    Create main.build.ps1

---
+++ main.build.ps1
@@ -0,0 +1,43 @@
+<#
+.Synopsis
+	Build script invoked by Invoke-Build.
+
+.Description
+
+#>
+
+param(
+)
+
+$data = Import-PowerShellDataFile -Path .\main.build.psd1
+
+task Test Build, {
+    $project = Resolve-Path -Path $data.TestProject
+    $project = $project.Path
+    Exec { dotnet test $project }
+    Exec { dotnet run --project $project }
+}
+
+task Build Format, {
+    $solution = Resolve-Path -Path $data.Solution
+    $solution = $solution.Path
+    Exec { dotnet restore $solution }
+    Exec { dotnet build $solution }
+}
+
+task Format Clean, {
+    Exec { dotnet tool restore }
+    Exec { dotnet fantomas . --recurse }
+}
+task Clean Init, {
+    $solution = Resolve-Path -Path $data.Solution
+    Exec { dotnet clean $solution }
+}
+
+task Init {
+    # $trashSubFolder = Get-Date -Format 'yyyyMMHHmmss'
+    # $script:trashFolder = Join-Path -Path . -ChildPath '.trash'
+    # $script:trashFolder = Join-Path -Path $script:trashFolder -ChildPath $trashSubFolder
+    # New-Item -Path $script:trashFolder -ItemType Directory | Out-Null
+    # $script:trashFolder = Resolve-Path -Path $script:trashFolder
+}
