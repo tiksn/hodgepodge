aa2afe8f6fe3ee641b5162485e530aceee7a1a92
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-06-20 01:52:08 +0300

    Update CrossPlatform.fs

--- hodgepodge/CrossPlatform.fs
+++ hodgepodge/CrossPlatform.fs
@@ -1,5 +1,7 @@
 module TIKSN.hodgepodge.CrossPlatform
 
+open Prime
+
 let makeNullAsNone x =
     match x with
     | Some null -> None
@@ -14,5 +16,41 @@ let setRootProcessParentToNone (p: ProcessInfo) =
             p
     | None -> p
 
+let findProcessClusters (list: ProcessInfo list) =
+    let rec shouldBeInCluster (item: ProcessInfo) (cluster: ProcessInfo list) =
+        match cluster with
+        | [] -> false
+        | x :: xs ->
+            if
+                item.Path.IsSome
+                && item.Path = x.Path
+                && (Some x.ProcessId = item.ParentProcessId
+                    || x.ParentProcessId = Some item.ProcessId)
+            then
+                true
+            else
+                shouldBeInCluster item xs
+
+    let rec clusterHelper (acc: ProcessInfo list list) currentCluster rest =
+        match rest with
+        | [] -> currentCluster :: acc
+        | x :: xs ->
+            if shouldBeInCluster x currentCluster then
+                clusterHelper acc (x :: currentCluster) xs
+            else
+                clusterHelper (currentCluster :: acc) [ x ] xs
+
+    let mapClusterToEitherList (cluster: ProcessInfo list) =
+        match cluster with
+        | [ x ] -> Left x
+        | _ -> Right cluster
+
+    let clusters =
+        match list with
+        | [] -> []
+        | x :: xs -> List.rev (clusterHelper [] [ x ] xs)
+
+    clusters |> List.map mapClusterToEitherList
+
 let groupProcesses (pil: ProcessInfo list) =
-    pil |> List.map setRootProcessParentToNone
+    pil |> List.map setRootProcessParentToNone |> findProcessClusters
