423c3f59cf42d6c1f5913e90607590063ba6acd7
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-06-21 02:43:43 +0300

    Update Platform.fs

--- hodgepodge/Platform.fs
+++ hodgepodge/Platform.fs
@@ -3,20 +3,20 @@
 open System
 open Prime
 
-let platformNotFoundError = pair 1469762974 "Platform not Found"
+let platformNotSupportedError = pair 1469762974 "Platform is not supported"
 
 let listProcess () : Result<Either<ProcessInfo, ProcessTreeInfo> list, (int * string)> =
     match Environment.OSVersion.Platform with
     | PlatformID.Win32NT -> Ok(Windows.listProcess ())
-    | _ -> Error platformNotFoundError
+    | _ -> Error platformNotSupportedError
     |> Result.map CrossPlatform.groupProcesses
 
 let listServices () : Result<ServiceInfo list, (int * string)> =
     match Environment.OSVersion.Platform with
     | PlatformID.Win32NT -> Ok(Windows.listServices ())
-    | _ -> Error platformNotFoundError
+    | _ -> Error platformNotSupportedError
 
 let listInstalledPrograms () : Result<InstalledProgramInfo list, (int * string)> =
     match Environment.OSVersion.Platform with
     | PlatformID.Win32NT -> Ok(Windows.listInstalledPrograms ())
-    | _ -> Error platformNotFoundError
+    | _ -> Error platformNotSupportedError
