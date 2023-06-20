a88dcb4fc29a2887dd7382666c0f3444c0abf06e
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-06-21 01:05:51 +0300

    Update CrossPlatform.fs

--- hodgepodge/CrossPlatform.fs
+++ hodgepodge/CrossPlatform.fs
@@ -16,45 +16,93 @@ let setRootProcessParentToNone (p: ProcessInfo) : ProcessInfo =
             p
     | None -> p
 
-let findProcessClusters (list: ProcessInfo list) : Either<ProcessInfo, ProcessInfo list> list =
-    let rec shouldBeInCluster (item: ProcessInfo) (cluster: ProcessInfo list) : bool =
-        match cluster with
-        | [] -> false
-        | x :: xs ->
-            if
-                item.Path.IsSome
-                && item.Path = x.Path
-                && (Some x.ProcessId = item.ParentProcessId
-                    || x.ParentProcessId = Some item.ProcessId)
-            then
-                true
-            else
-                shouldBeInCluster item xs
+let findProcessClusters (list: ProcessInfo list) : Either<ProcessInfo, ProcessTreeInfo> list =
+    let shouldBeClusterParent (item: ProcessInfo) (cluster: ProcessTreeInfo) : bool =
+        item.Path.IsSome
+        && item.Path = cluster.Path
+        && (Some item.ProcessId = cluster.ParentProcessId)
 
-    let rec clusterHelper
-        (acc: ProcessInfo list list)
-        (currentCluster: ProcessInfo list)
-        (rest: ProcessInfo list)
-        : ProcessInfo list list =
-        match rest with
-        | [] -> currentCluster :: acc
-        | x :: xs ->
-            if shouldBeInCluster x currentCluster then
-                clusterHelper acc (x :: currentCluster) xs
-            else
-                clusterHelper (currentCluster :: acc) [ x ] xs
+    let shouldBeClusterChild (item: ProcessInfo) (cluster: ProcessTreeInfo) : bool =
+        item.Path.IsSome
+        && item.Path = cluster.Path
+        && (Some cluster.ProcessId = item.ParentProcessId)
 
-    let mapClusterToEitherList (cluster: ProcessInfo list) : Either<ProcessInfo, ProcessInfo list> =
-        match cluster with
-        | [ x ] -> Left x
+    let rec addToCluster (item: ProcessInfo) (cluster: ProcessTreeInfo) : ProcessTreeInfo option =
+        if shouldBeClusterParent item cluster then
+            Some
+                { ProcessId = item.ProcessId
+                  ParentProcessId = item.ParentProcessId
+                  Path = item.Path
+                  Children = [ cluster ] }
+        elif shouldBeClusterChild item cluster then
+            Some
+                { cluster with
+                    Children =
+                        ({ ProcessId = item.ProcessId
+                           ParentProcessId = item.ParentProcessId
+                           Path = item.Path
+                           Children = [] }
+                         :: cluster.Children) }
+        else
+            match cluster.Children with
+            | [] -> None
+            | _ ->
+                (let changedChildren =
+                    cluster.Children
+                    |> List.map (fun x ->
+                        let newChild = addToCluster item x
+
+                        match newChild with
+                        | Some c ->
+                            { cluster with
+                                Children = (c :: cluster.Children) }
+                        | None -> x)
+
+                 if cluster.Children <> changedChildren then
+                     Some
+                         { cluster with
+                             Children = changedChildren }
+                 else
+                     None)
+
+    let mapProcessInfoToCluster (pi: ProcessInfo) : ProcessTreeInfo =
+        { ProcessId = pi.ProcessId
+          ParentProcessId = pi.ParentProcessId
+          Path = pi.Path
+          Children = [] }
+
+    let mapClusterToEither (cluster: ProcessTreeInfo) : Either<ProcessInfo, ProcessTreeInfo> =
+        match cluster.Children with
+        | [] ->
+            Left
+                { ProcessId = cluster.ProcessId
+                  ParentProcessId = cluster.ParentProcessId
+                  Path = cluster.Path }
         | _ -> Right cluster
 
-    let clusters =
-        match list with
-        | [] -> []
-        | x :: xs -> List.rev (clusterHelper [] [ x ] xs)
+    let rec clusterHelper
+        (acc: Either<ProcessInfo, ProcessTreeInfo> list)
+        (currentCluster: ProcessTreeInfo)
+        (rest: ProcessInfo list)
+        : Either<ProcessInfo, ProcessTreeInfo> list =
+        match rest with
+        | [] -> mapClusterToEither (currentCluster) :: acc
+        | x :: xs ->
+            let newCluster = addToCluster x currentCluster
 
-    clusters |> List.map mapClusterToEitherList
+            match newCluster with
+            | Some c -> clusterHelper acc c xs
+            | None -> clusterHelper (mapClusterToEither (currentCluster) :: acc) (mapProcessInfoToCluster x) xs
 
-let groupProcesses (pil: ProcessInfo list) : Either<ProcessInfo, ProcessInfo list> list =
+    match list with
+    | [] -> []
+    | x :: xs ->
+        List.rev (
+            clusterHelper
+                []
+                (mapProcessInfoToCluster x)
+                xs
+        )
+
+let groupProcesses (pil: ProcessInfo list) : Either<ProcessInfo, ProcessTreeInfo> list =
     pil |> List.map setRootProcessParentToNone |> findProcessClusters
